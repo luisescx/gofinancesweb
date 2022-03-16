9a0d5567341d3b0f5fe6d84e4672118922477cd1
luisescx <luis.esculapio@live.com>
2022-03-16 20:07:39 -0300

    chore: added header component on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div >
-      <h1>hello world</h1>
-    </div>
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
   );
 }
-
-export default App;
